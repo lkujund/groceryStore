55a954e3caf99f19f5e086303fc87b9063a9150f
Luka K <lkujund@racunarstvo.hr>
2024-09-09 16:46:50 +0200

    Changes 4

--- src/main/java/hr/grocery/store/grocerystore/model/Order.java
+++ src/main/java/hr/grocery/store/grocerystore/model/Order.java
@@ -1,10 +1,7 @@
 package hr.grocery.store.grocerystore.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 import java.util.List;
@@ -15,6 +12,7 @@ import java.util.List;
 @Table(name = "ORDERS")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
